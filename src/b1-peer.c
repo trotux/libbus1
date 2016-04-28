3684da05c0194f21fe98e0d9ec654566a4202595
David Herrmann <dh.herrmann@gmail.com>
2016-04-28 17:40:26 +0200

    b1-peer: order includes alphabetically
    Keep alphabetical include order, as we do everywhere else.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- src/b1-peer.c
+++ src/b1-peer.c
@@ -16,11 +16,11 @@
 ***/
 
 #include <assert.h>
+#include <c-rbtree.h>
+#include <c-variant.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
-#include <c-rbtree.h>
-#include <c-variant.h>
 #include "bus1-client.h"
 #include "org.bus1/b1-peer.h"
 #include "org.bus1/c-macro.h"
