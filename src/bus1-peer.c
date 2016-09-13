717b601ba4f0dd5d6feafb99e7fd7baf2292ddec
Alexander Alemayhu <alexander@alemayhu.com>
2016-09-13 10:55:27 +0200

    build: update copyright notice (#3)
    This is inspired by commit 0fe54d4 (build: update copyright notice, 2016-09-09)
    on bus1.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- src/bus1-peer.c
+++ src/bus1-peer.c
@@ -1,10 +1,10 @@
 /*
  * Copyright (C) 2013-2016 Red Hat, Inc.
  *
- * bus1 is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at
- * your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  */
 
 #include <assert.h>
--- src/test-peer.c
+++ src/test-peer.c
@@ -1,10 +1,10 @@
 /*
- * This file is part of bus1. See COPYING for details.
+ * Copyright (C) 2013-2016 Red Hat, Inc.
  *
- * bus1 is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  */
 
 /*
